c011d5d8c15ab7fb8199387eabbe579c91b6c8b2
Gabrieldarri <gabrielbjarnason@gmail.com>
2025-05-20 12:36:27 +0200

    plaaaaeeeesseeeaa

--- Test-GO-Catalog/obj/Debug/net9.0/Test-GO-Catalog.AssemblyInfo.cs
+++ Test-GO-Catalog/obj/Debug/net9.0/Test-GO-Catalog.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test-GO-Catalog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83471c70ea9331595739198165df736f6bab28a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c561cef8c88abc367dbd9cab50a9f55c044e4649")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test-GO-Catalog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test-GO-Catalog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
